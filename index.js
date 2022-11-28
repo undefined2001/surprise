8b6eb106a545aa637bb16c82559ec8d35162dbdf
Undefined <116953958+undefined2001@users.noreply.github.com>
2022-11-28 16:37:44 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let add = document.getElementById("add");
+let out = document.getElementById("output")
+
+add.addEventListener("click", () => {
+    let name = document.getElementById("name").value;
+    if(name != ""){
+    out.innerHTML = `
+    <h1>I LOVE YOU</h1>
+    <h1 class="name">${name.toUpperCase()}</h1>
+    ` ;
+    }else{
+        out.innerHTML =""
+    }
+    
+});
