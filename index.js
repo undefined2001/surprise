b81caed380da2ae4588af50543478815575d0af4
Undefined <116953958+undefined2001@users.noreply.github.com>
2022-12-14 03:09:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ let out = document.getElementById("output")
 
 add.addEventListener("click", () => {
     let name = document.getElementById("name").value;
-    if(name != ""){
+    if(name.toUpperCase() === "RIFAT"){
+           out.innerHTML = `<h1>FUCK YOU</h1>
+    <h1 class="name">${name.toUpperCase()}</h1>
+    ` ;
+   
+    }else if(name != ""){
     out.innerHTML = `
     <h1>I LOVE YOU</h1>
     <h1 class="name">${name.toUpperCase()}</h1>
     ` ;
-    }else if(name.toUpperCase() === "RIFAT"){
-          out.innerHTML = `
-    <h1>FUCK YOU</h1>`;
     }
     else{
         out.innerHTML =""
