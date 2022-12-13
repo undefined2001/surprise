646f7564811684117ebab317232fec760ee7f550
Undefined <116953958+undefined2001@users.noreply.github.com>
2022-12-14 03:14:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let add = document.getElementById("add");
 let out = document.getElementById("output")
-
+let friends = ["RIFAT", "TAMIM", "REDWAN"]
 add.addEventListener("click", () => {
     let name = document.getElementById("name").value;
-    if(name.toUpperCase() === "RIFAT"){
+    if(name.toUpperCase() in friends){
            out.innerHTML = `<h1>FUCK YOU</h1>
     <h1 class="name">${name.toUpperCase()}</h1>
     ` ;
